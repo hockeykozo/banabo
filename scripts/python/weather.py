27a1e4ad8d92dc55298ebaa5857e6841016c2323
takashi <takashi@development.cloudnine.internal>
2016-05-08 20:50:07 +0000

    cleanup

--- scripts/python/weather.py
+++ scripts/python/weather.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     # print date
     print_date(num)
     # print weather
-    print_weather(tokyo_weather,num)
     print_weather(manila_weather,num)
+    print_weather(tokyo_weather,num)
 
 #    print json.dumps(tokyo_weather, separators=(',',':'), indent=4)
